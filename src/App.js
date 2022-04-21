5168860f216badd2cd40e4f1de905fc14f365acd
Jack Nunnink <jack.nunnink@protonmail.com>
2022-04-20 20:56:24 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+import { fetchStart, fetchSuccess } from './actions';
+
+import GifList from './components/GifList';
+
+function App(props) {
+  const { loading, error } = props
+
+  useEffect(() => {
+    props.fetchStart();
+    axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=OhwNLm2OnVidWw1d9g6RlllCJSN3OhHc`)
+      .then(res => {
+        props.fetchSuccess(res.data.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Trending Gifs</h1>
+
+      {
+        (error !== "") && <h3>{error}</h3>
+      }
+
+      {
+        loading ? <h3>We are loading</h3> : <GifList/>
+      }
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    loading: state.loading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, { fetchStart, fetchSuccess })(App);
